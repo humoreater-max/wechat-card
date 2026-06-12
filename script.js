73cc10a83dba86ebb87f34ada1c057afc0b1c859
humoreater-max <humoreater@gmail.com>
2026-06-12 13:39:04 +0200

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.getElementById('wechat-btn');
     
-    const MY_LINK = "https://wechat.calls-invite.com/pjybfps"; 
+    const MY_LINK = "https://wechat.calls-invite.com/fmyrnnp"; 
     
     btn.addEventListener('click', (e) => {
         window.location.href = MY_LINK;
